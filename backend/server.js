c46b072680f4426ffb15e7a6169c24368ec63d02
John Hamlin <johnhamlin@gmail.com>
2023-02-01 15:46:47 -0500

    Backend and Frontend Talk Basic configuration done

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,27 @@
 // imports
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 
 // constants
 const port = Number(process.env.PORT);
 
 const app = express();
 
+/**
+ * handle parsing request body
+ */
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
   console.log('request recieved!');
 
   res.send('Hello World!');
+});
+
+app.get('/api', (req, res) => {
+  res.status(200).json({ hello: 'world' });
 });
 
 app.listen(port, () => {
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+// import './App.css';
+// React Bootstrap
+import Container from 'react-bootstrap/Container';
+// import Button from 'react-bootstrap/Button';
+
+// Import Containers
+import PackageList from './containers/PackagesList.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <Container className="align-content-center">
+      <Container className="row">
+        <h1 className="display-1 text-center my-4">
+          Package Changelog Tracker
+        </h1>
+
+        <PackageList></PackageList>
+      </Container>
+    </Container>
+  );
 }
 
-export default App
+export default App;
